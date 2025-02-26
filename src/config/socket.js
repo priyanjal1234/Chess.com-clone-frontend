75bac62f3118a245d3979652fc11a62f385033c7
priyanjal1234 <priyanjal362@gmail.com>
2025-02-26 13:10:33 +0530

    Update socket.js

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000",{
+const socket = io("https://chess-com-clone-backend.onrender.com",{
     withCredentials: true,
     autoConnect: false
 })
